dcdbbe46ea3d21f44dcb9b3a7bbf57dfd76a411a
nithish125 <nithishs3620@gmail.com>
2022-08-25 21:33:00 +0530

    add

---
+++ dic2.py
@@ -0,0 +1,12 @@
+c=[]
+for x in range(5):
+    name= input("enter name :")
+    age= input("enter age :")
+    address= input("enter adress :")
+    # name3= input("enter name :")
+    b={"name":name ,"age":age ,"address":address}
+    c.append(b)
+
+print(c)
+    
+
